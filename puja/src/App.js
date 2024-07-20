92f90936e37898dcf313ce6020fc4c5e47e7eaa3
avantidhande <dhandeavanti22gmail.com>
2024-07-20 15:58:29 +0530

    pricing table

--- puja/src/App.js
+++ puja/src/App.js
@@ -27,6 +27,7 @@ function App() {
     autoplay: true,
     autoplaySpeed: 3000,
   };
+  
   return (
     <div className="App">
       <NavigationBar />
@@ -55,6 +56,39 @@ function App() {
           </p>
         </div>
       </section>
+
+
+      <section id="pricing" className="pricing-section">
+        <h1 className="text-center my-5">Pricing</h1>
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card text-center pricing-card">
+                <div className="card-body">
+                  <h2 className="card-title">Basic</h2>
+                  <h1 className="card-price">₹5/Month</h1>
+                  <p className="card-text">Access to basic features</p>
+                  <p className="card-text">Email support</p>
+                  <button className="btn btn-primary">Order Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card text-center pricing-card">
+                <div className="card-body">
+                  <h2 className="card-title">Advanced</h2>
+                  <h1 className="card-price">₹10/Month</h1>
+                  <p className="card-text">Access to all features</p>
+                  <p className="card-text">Priority support</p>
+                  <button className="btn btn-primary">Order Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
 
       <section id="services" className="services-section">
         <div className="service-card">
