a00dd7a90e64ff8ef98bf2f4384a96ebce24b0f9
nirajsabre04 <nirajsabre04@gmail.com>
2024-07-20 11:13:17 +0530

    .

--- puja/src/Component/Navbar.js
+++ puja/src/Component/Navbar.js
@@ -7,7 +7,7 @@ const NavigationBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Your Logo</Navbar.Brand>
+        <Navbar.Brand href="#home">PUJA SAMAGRI</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
