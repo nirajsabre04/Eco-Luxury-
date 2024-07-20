7582cffe60a0e1630df834d2f44080e4f1be8fcc
Aman Marve <Aman1.marve@gmail.com>
2024-07-20 17:05:46 +0530

    commit

--- puja/src/App.js
+++ puja/src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Home from './Pages/Home';
-
+ 
 function App() {
   return (
     <div className="App">
