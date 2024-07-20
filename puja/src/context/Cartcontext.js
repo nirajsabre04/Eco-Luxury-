ec695a8171f1091b7c832e5189f20c3ba5f12ac8
Aman Marve <Aman1.marve@gmail.com>
2024-07-20 16:25:18 +0530

    no error

--- puja/src/context/Cartcontext.js
+++ puja/src/context/Cartcontext.js
@@ -1,35 +1,18 @@
-// src/contexts/CartContext.js
-import React, { createContext, useReducer } from 'react';
+// src/CartContext.js
+import React, { createContext, useState } from 'react';
 
-const CartContext = createContext();
-
-const cartReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TO_CART':
-      return [...state, action.payload];
-    case 'REMOVE_FROM_CART':
-      return state.filter(item => item.id !== action.payload.id);
-    default:
-      return state;
-  }
-};
+export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, dispatch] = useReducer(cartReducer, []);
+  const [cart, setCart] = useState([]);
 
-  const addToCart = item => {
-    dispatch({ type: 'ADD_TO_CART', payload: item });
-  };
-
-  const removeFromCart = item => {
-    dispatch({ type: 'REMOVE_FROM_CART', payload: item });
+  const addToCart = (item) => {
+    setCart([...cart, item]);
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cart, addToCart }}>
       {children}
     </CartContext.Provider>
   );
 };
-
-export default CartContext;
