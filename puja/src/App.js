b835491c7e7dfbef93f387a0d369bac86beac0b8
nirajsabre04 <nirajsabre04@gmail.com>
2024-07-20 14:07:42 +0530

    service

--- puja/src/App.js
+++ puja/src/App.js
@@ -5,6 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Hero from '../src/assets/Hero.mp4'
 import aboutImage from '../src/assets/about.png'
+import S1 from '../src/assets/S1.webp'
+import S2 from '../src/assets/S2.webp'
+import S3 from '../src/assets/S3.gif'
 
 function App() {
   return (
@@ -33,6 +36,24 @@ function App() {
             made from natural and organic materials, ensuring a clean and green
             environment for your everyday needs.
           </p>
+        </div>
+      </section>
+
+      <section id="services" className="services-section">
+        <div className="service-card">
+          <img src={S1} alt="Service 1" />
+          <h3>High-Quality</h3>
+          <p>High-quality cow dung cakes for a sustainable living.</p>
+        </div>
+        <div className="service-card">
+          <img src={S3} alt="Service 2" />
+          <h3>Eco-Friendly</h3>
+          <p>Eco-friendly and natural materials for your everyday needs.</p>
+        </div>
+        <div className="service-card">
+          <img src={S2} alt="Service 3" />
+          <h3>Sustainable</h3>
+          <p>Clean and green environment with our sustainable products.</p>
         </div>
       </section>
   
