da84325c6ab92df580d6a7d15787551afcb7e739
nirajsabre04 <nirajsabre04@gmail.com>
2024-07-20 11:55:46 +0530

    b

--- puja/src/App.js
+++ puja/src/App.js
@@ -3,13 +3,23 @@ import React from 'react';
 import NavigationBar from '../src/Component/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Hero from '../src/assets/Hero.mp4'
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
       <section id="home">
-        <h1>Home</h1>
+        <div className="video-container">
+          <video autoPlay loop muted>
+            <source src={Hero} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <div className="overlay-text">
+            <h1>Natural Fuel for a Greener Tomorrow</h1>
+            <p>Eco-friendly Cow Dung Cakes for Sustainable Living</p>
+          </div>
+        </div>
       </section>
   
     </div>
