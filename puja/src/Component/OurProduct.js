c0c50155d70619d4f6c51baadd31646f30f37dad
Aman Marve <Aman1.marve@gmail.com>
2024-07-22 18:01:11 +0530

    haha

--- puja/src/Component/OurProduct.js
+++ puja/src/Component/OurProduct.js
@@ -22,15 +22,23 @@ const MySwiper = () => {
       >
         <SwiperSlide>
           <img src={final.Img1} className="" alt="Product 1" />
+          <p id="desc">Embrace the ancient traditions of India with these organic pure cow dung gobar upla/uplay cakes</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={final.Img2} className="" alt="Product 2" />
+          <p id="desc">Made from 100% pure and natural Indian cow dung, these cakes are meticulously handcrafted with due care and process</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={final.Img3} className="" alt="Product 3" />
+          <p id="desc">Completely dried and moisture-free, they burn properly, making them ideal for hawan, puja, and other religious purposes</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={final.Img4} className="" alt="Product 4" />
+          <p id="desc"> Embrace the purity and authenticity of these organic cow dung cakes and elevate your religious ceremonies with their sacred essence.</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={final.Img5} className="" alt="Product 4" />
+          <p id="desc">Embrace the ancient traditions of India with these organic pure cow dung gobar upla/uplay cakes</p>
         </SwiperSlide>
       </Swiper>
     </div>
--- puja/src/assets/Final.js
+++ puja/src/assets/Final.js
@@ -2,10 +2,12 @@ import Img1 from './3.png'
 import Img2 from './4.png'
 import Img3 from './5.png'
 import Img4 from './6.png'
+import Img5 from './2.png'
 
 export const final ={
     Img1,
     Img2,
     Img3,
     Img4,
+    Img5
 }
