04a500491df7f4cee97ec00ccefa8f7782d7d2f9
nirajsabre04 <nirajsabre04@gmail.com>
2024-07-23 10:54:11 +0530

    nav

--- puja/src/Component/Navbar.js
+++ puja/src/Component/Navbar.js
@@ -3,21 +3,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Logo from '../assets/pujasamagri.online.png'
+import Logo from '../assets/pujasamagri.online.png';
+
 const NavigationBar = () => {
   return (
     <Navbar bg="white" variant="light" expand="lg">
       <Container>
-        <Navbar.Brand to='/'><img    style={{
-            width: '65px',    // Adjust size as needed
-            height: '65px',   // Adjust size as needed
-            marginRight: '10px' // Space between logo and text
-          }} src={Logo} />PUJA SAMAGRI</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          <img
+            style={{
+              width: '65px', // Adjust size as needed
+              height: '65px', // Adjust size as needed
+              marginRight: '10px' // Space between logo and text
+            }}
+            src={Logo}
+            alt="Puja Samagri Logo"
+          />
+          <span style={{ color: '#ff6600', fontWeight: 'bold' }}>PUJA SAMAGRI</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to='/'>Home</Nav.Link>
-            <Nav.Link as={Link} to='/about'>About</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
             <Nav.Link as={Link} to="/services">Services</Nav.Link>
             <Nav.Link as={Link} to="/testimonials">Testimonials</Nav.Link>
