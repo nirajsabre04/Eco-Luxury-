764871a16b266c702873108f7f7fb15063df50b8
Aman Marve <Aman1.marve@gmail.com>
2024-08-22 13:36:35 +0530

    CSS

--- puja/src/Pages/Home.js
+++ puja/src/Pages/Home.js
@@ -8,10 +8,17 @@ import TestimonialsSection from '../Component/TestimonialsSection';
 import ContactForm from '../Component/ContactForm';
 import BenefitsSlider from '../Component/BenefitsSlider';
 import OurProduct from '../Component/OurProduct';
+import '../CSS/Home.css'
+
 
 const Home = () => {
   return (
     <div>
+         <div className='whatsapp_float'>
+        <a href='https://wa.me/1234567890'>
+        <img src='https://nareshit.com/images/whatsapp.svg' alt='WhatsApp' />
+        </a>
+      </div>
       <HeroSection />
       <PricingSection />
       <AboutSection />
