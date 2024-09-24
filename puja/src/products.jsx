81f809665b08d6fe2742b8fd937dae6e7d64a0ee
AmanMarve <Aman1.marve@gmail.com>
2024-09-24 14:52:36 +0530

    CSS

--- puja/src/products.jsx
+++ puja/src/products.jsx
@@ -12,11 +12,11 @@ export const productsData = [
           "A delightful floral aroma of Rose for calming your senses.",
         subImages: [
           // Sub-images for Rose flavor
-          require("./assets/ROSE/B.png"),
-          require("./assets/ROSE/C.png"),
-          require("./assets/ROSE/D.png"),
-          require("./assets/ROSE/E.png"),
-          require("./assets/ROSE/F.png"),
+          require("./assets/ROSE/A1.png"),
+          require("./assets/ROSE/A2.png"),
+          require("./assets/ROSE/A3.png"),
+          require("./assets/ROSE/A4.png"),
+          require("./assets/ROSE/A5.png"),
         ],
       },
       {
@@ -26,11 +26,11 @@ export const productsData = [
           "Guggle flavor offers a rich and earthy fragrance for a meditative atmosphere.",
         subImages: [
           // Sub-images for Guggle flavor
-          require("./assets/GUGGAL/D.png"),
-          require("./assets/GUGGAL/E.png"),
-          require("./assets/GUGGAL/F.png"),
-          require("./assets/GUGGAL/G.png"),
-          require("./assets/GUGGAL/H.png"),
+          require("./assets/GUGGAL/C1.png"),
+          require("./assets/GUGGAL/C2.png"),
+          require("./assets/GUGGAL/C3.png"),
+          require("./assets/GUGGAL/C4.png"),
+          require("./assets/GUGGAL/C5.png"),
         ],
       },
       {
@@ -40,11 +40,11 @@ export const productsData = [
           "A soothing and woody aroma of Sandalwood, perfect for relaxation.",
         subImages: [
           // Sub-images for Sandalwood flavor
-          require("./assets/SANDALWOOD/E.png"),
-          require("./assets/SANDALWOOD/F.png"),
-          require("./assets/SANDALWOOD/G.png"),
-          require("./assets/SANDALWOOD/H.png"),
-          require("./assets/SANDALWOOD/I.png"),
+          require("./assets/SANDALWOOD/D1.png"),
+          require("./assets/SANDALWOOD/D2.png"),
+          require("./assets/SANDALWOOD/D3.png"),
+          require("./assets/SANDALWOOD/D4.png"),
+          require("./assets/SANDALWOOD/D5.png"),
         ],
       },
       {
@@ -54,11 +54,11 @@ export const productsData = [
           "Loban flavor creates a deep and mystical ambiance with its unique scent.",
         subImages: [
           // Sub-images for Loban flavor
-          require("./assets/LOBAN/G.png"),
-          require("./assets/LOBAN/H.png"),
-          require("./assets/LOBAN/I.png"),
-          require("./assets/LOBAN/J.png"),
-          require("./assets/LOBAN/K.png"),
+          require("./assets/LOBAN/F1.png"),
+          require("./assets/LOBAN/F2.png"),
+          require("./assets/LOBAN/F3.png"),
+          require("./assets/LOBAN/F4.png"),
+          require("./assets/LOBAN/F5.png"),
         ],
       },
       {
@@ -68,11 +68,11 @@ export const productsData = [
           "A sweet and exotic Jasmine fragrance for a refreshing environment.",
         subImages: [
           // Sub-images for Jasmine flavor
-          require("./assets/MOGRA/C.png"),
-          require("./assets/MOGRA/D.png"),
-          require("./assets/MOGRA/E.png"),
-          require("./assets/MOGRA/F.png"),
-          require("./assets/MOGRA/G.png"),
+          require("./assets/MOGRA/B1.png"),
+          require("./assets/MOGRA/B2.png"),
+          require("./assets/MOGRA/B3.png"),
+          require("./assets/MOGRA/B4.png"),
+          require("./assets/MOGRA/B5.png"),
         ],
       },
     ],
